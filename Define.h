943eaefe92b8214b42025db4f4d31b7db2210cbb
Edwin Chen <edwin14chen@gmail.com>
2024-12-08 22:40:07 -0500

    Check existence of inputted folder path

---
+++ Define.h
@@ -0,0 +1,9 @@
+#ifndef CORE_H
+#define CORE_H
+
+// keep these in alphabetical order
+#include <dirent.h> // opendir
+#include <errno.h> // errno
+#include <stdio.h> // printf, scanf
+
+#endif // !CORE_H
--- main.c
+++ main.c
@@ -1,7 +1,29 @@
-#include "Core.h"
+#include "Define.h"
 
 int main()
 {
-    HelloWorld();
+    char filePath[512];
+
+    printf("Enter the desired filepath: ");
+
+    scanf("%s", filePath);
+
+    printf("You entered: %s\n", filePath);
+
+    DIR* pdir = opendir(filePath);
+    if (pdir)
+    {
+        printf("Directory (%s) was found!", filePath);
+        closedir(pdir);
+    }
+    else if (ENOENT == errno)
+    {
+        printf("Directory (%s) was not found.", filePath);
+    }
+    else
+    {
+        printf("opendir() failed for unknown reason.");
+    }
+
     return 0;
 }
